13db40037554e56361a839c0ca86b106442baf1e
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-02-07 22:37:42 -0300

    🔮 arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod display;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "MyNotes", about = "A Daily Note Reminder")]
+struct Opt {
+    #[structopt(short = "n", long = "new", help = "Create a new note")]
+    new: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    display::help();
+    
+    let opt = Opt::from_args();
+    println!("{:?}", opt.new);
 }
